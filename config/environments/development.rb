2a9ad128fd946f10b65e72a01ea80bfd8b842288
That4123 <tranmauthat@gmail.com>
2024-06-12 10:07:37 +0700

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   # config.host.clear
+  config.action_mailer.raise_delivery_errors = false
+  host = 'localhost:3000'
+# Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 # Create a main sample user.
 require 'faker'
-User.create!(name: "Tran Mau That", email: "that.tran4123@hcmut.edu.vn", password: "04102003", password_confirmation: "04102003",admin: true)
+User.create!(name: "Tran Mau That", email: "that.tran4123@hcmut.edu.vn", password: "04102003", 
+            password_confirmation: "04102003",admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
     email = "example_#{n+1}@railstutorial.org"
     password = "password"
     # name = "User Number #{n+1}"
     name = Faker::Name.name
-    User.create!(name: name, email: email, password: password, password_confirmation: password)
+    User.create!(name: name, email: email, password: password, password_confirmation: password,
+                    activated: true, activated_at: Time.zone.now)
 end
